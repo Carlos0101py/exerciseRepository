35c5ef203ba95c1efe12eff1c503c9a16b019577
Carlos Erick Mota Erick <73728755+Carlos0101py@users.noreply.github.com>
2023-03-30 03:13:00 +0000

    Initial commit

--- main.c
+++ main.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 
-//1–Faça um programa que leia três valores (A, B, C) e mostre-os na ordem lida.  Em seguida, mostre-os em ordem crescente e decrescente.
-
+// 1–Faça um programa que leia três valores (A, B, C) e mostre-os na ordem lida.
+// Em seguida, mostre-os em ordem crescente e decrescente.
 
 int main(void) {
 
   int vetNumero[3];
 
-  //leitura
-  for(int i = 0; i < 3; i++){
+  // leitura
+  for (int i = 0; i < 3; i++) {
     printf("Digite um numero: ");
     scanf("%d", &vetNumero[i]);
   }
-  //ordem lida
+  // ordem lida
   printf("ordem lida: ");
-  for(int i = 0; i < 3; i++){
+  for (int i = 0; i < 3; i++) {
     printf("%d ", vetNumero[i]);
   }
   printf("\n");
   printf("Crescente: ");
-  //ordem crescente
-  for(int i = 0; i < 3; i++){
-    for(int j = i + 1; j < 3; j++){
-      if(vetNumero[i] >= vetNumero[j]){
+  // ordem crescente
+  for (int i = 0; i < 3; i++) {
+    for (int j = i + 1; j < 3; j++) {
+      if (vetNumero[i] >= vetNumero[j]) {
         int aux = vetNumero[i];
         vetNumero[i] = vetNumero[j];
         vetNumero[j] = aux;
       }
     }
   }
-  for(int i = 0; i < 3; i++){
+  for (int i = 0; i < 3; i++) {
     printf("%d ", vetNumero[i]);
   }
-   printf("\n");
+  printf("\n");
   printf("decrescente: ");
-  //ordem decrecente
-  for(int i = 0; i < 3; i++){
-    for(int j = i + 1; j < 3; j++){
-      if(vetNumero[i] <= vetNumero[j]){
+  // ordem decrecente
+  for (int i = 0; i < 3; i++) {
+    for (int j = i + 1; j < 3; j++) {
+      if (vetNumero[i] <= vetNumero[j]) {
         int aux = vetNumero[i];
         vetNumero[i] = vetNumero[j];
         vetNumero[j] = aux;
       }
     }
   }
-  for(int i = 0; i < 3; i++){
+  for (int i = 0; i < 3; i++) {
     printf("%d ", vetNumero[i]);
   }
   return 0;
